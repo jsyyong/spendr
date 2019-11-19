c11b9bb68411b64298f1375d91ac96147b1eb2dd
rkmaboy <51350957+rkmaboy@users.noreply.github.com>
2019-11-19 12:13:45 -0500

    added some endpoints and comments

--- server.js
+++ server.js
@@ -2,20 +2,26 @@ let express = require("express");
 let app = express();
 let reloadMagic = require("./reload-magic.js");
 let multer = require("multer"); ///handles http requests from form submissions. It places data in the body property of the request object
-let upload = multer(); // we will be uploading images, signing up and logging in
+let upload = multer({ dest: __dirname + "/uploads/" }); // we will be uploading images, signing up and logging in
 let MongoClient = require("mongodb").MongoClient; //used to initiate connection
 ObjectID = require("mongodb").ObjectID; //convert string to objectID
 reloadMagic(app);
 
 app.use("/", express.static("build")); // Needed for the HTML and JS files
-app.use("/", express.static("public")); // Needed for local assets
+app.use("/uploads", express.static("uploads")); // Needed for local assets
 let dbo = undefined;
 let url =
   "mongodb+srv://jeff:pwd123@cluster0-uckdj.mongodb.net/test?retryWrites=true&w=majority";
-MongoClient.connect(url, { useNewUrlParser: true }, (err, db) => {
-  dbo = db.db("media-board");
-});
+MongoClient.connect(
+  url,
+  { useNewUrlParser: true, useUnifiedTopology: true },
+  (err, db) => {
+    dbo = db.db("media-board");
+  }
+);
 // Your endpoints go after this line
+
+//Login Endpoint
 app.post("/login", upload.none(), (req, res) => {
   console.log("login", req.body);
   let name = req.body.username;
@@ -40,13 +46,8 @@ app.post("/login", upload.none(), (req, res) => {
     res.send(JSON.stringify({ success: false }));
   });
 });
-// Your endpoints go before this line
 
-app.all("/*", (req, res, next) => {
-  // needed for react router
-  res.sendFile(__dirname + "/build/index.html");
-});
-
+//Signup Endpoint
 app.post("/signup", upload.none(), (req, res) => {
   console.log("signup", req.body); //body is the data from signup from post component
   let name = req.body.username;
@@ -67,6 +68,27 @@ app.post("/signup", upload.none(), (req, res) => {
     res.send(JSON.stringify({ success: false }));
   });
 });
+
+//Upload Form Endpoint
+app.post("/new-upload", upload.single("img"), (req, res) => {
+  console.log("request to /new-post. body: ", req.body);
+  let description = req.body.description;
+  let file = req.file;
+  let frontendPath = "/uploads/" + file.filename;
+  dbo
+    .collection("posts")
+    .insertOne({ description: description, frontendPath: frontendPath });
+  res.send(JSON.stringify({ success: true }));
+});
+
+//All Images Endpoint
+// Your endpoints go before this line
+
+app.all("/*", (req, res, next) => {
+  // needed for react router
+  res.sendFile(__dirname + "/build/index.html");
+});
+
 app.listen(4000, "0.0.0.0", () => {
   console.log("Server running on port 4000");
 });
