1090ef34064650af7910b970159c7ca6fc07c6d4
Jesse Katabarwa <mail@jessekatabarwa.info>
2019-11-19 13:08:08 -0500

    testing p tag in userhomepage

--- src/UserHomePage.jsx
+++ src/UserHomePage.jsx
@@ -2,42 +2,41 @@ import React, { Component } from "react";
 import { connect } from "react-redux";
 
 class User extends Component {
-    constructor() {
-        
-        super(); 
-        this.state = {
-          username: this.state.username, 
-          post: []
-        }; 
-      }
-      sendingUsername = async () => {
-let name = this.state.username
-let data = new FormData();
-data.append("username", name);
-let response = await fetch("/image-userPage", {method:"POST", body:data});
-let body = await response.text();
-console.log("/image-userPage response", body); // 8
-body = JSON.parse(body);
-      }
-      
-      reload = async () => {
-        let response = await fetch("/image-userPage");
-        let body = await response.text();
-        console.log("/image-userPage response", body);
-        body = JSON.parse(body);
-        this.setState({ posts: body });
-      };
-      componentDidMount = () => {
-        this.sendingUsername();
-        this.reload();
-      };
-      render = () => {
-        return (
-          <div>
-              {this.state.posts.reverse().map(p => (
-                <div{p._id} contents={p} />
-              ))}
-          </div>
-        );
-      };
+  constructor() {
+    super();
+    this.state = {
+      username: this.state.username,
+      post: []
+    };
+  }
+  sendingUsername = async () => {
+    let name = this.state.username;
+    let data = new FormData();
+    data.append("username", name);
+    await fetch("/image-userPage", { method: "POST", body: data });
+  };
+
+  reload = async () => {
+    let response = await fetch("/image-userPage");
+    let body = await response.text();
+    console.log("/image-userPage response", body);
+    body = JSON.parse(body);
+    this.setState({ posts: body });
+  };
+  componentDidMount = () => {
+    this.sendingUsername();
+    this.reload();
+  };
+  render = () => {
+    return (
+      <div>
+          <p>test</p>
+      </div>
+      <div>
+        {this.state.posts.reverse().map(p => (
+          <div>{p} </div>
+        ))}
+      </div>
+    );
+  };
 }
